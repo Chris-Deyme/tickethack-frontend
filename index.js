e698ee1e271427205aff0a46c38734198366b381
Lola <loladupel@icloud.com>
2024-01-31 17:20:49 +0100

    last

--- index.js
+++ index.js
@@ -26,31 +26,57 @@ document.querySelector('#btn-search').addEventListener('click', function formCom
 })
 
 function fetchTrips(departure, arrival, date) {
-    fetch('../trips', {
-        method: 'Post',
+    console.log(JSON.stringify({
+            departure, arrival, date
+        }))
+    fetch('http://localhost:3000/trip', {
+        method: 'POST',
+        headers:{'Content-Type':'application/json'},
         body: JSON.stringify({
-            departure: departure,
-            arrival: arrival,
-            date: date
+            departure, arrival, date
         })
     }).then(response=>{
         if(!response.ok) {
-            throw new Error('Une erreur s\'est produite lors de la récupération des trajets.');
+            // throw new Error('Une erreur s\'est produite lors de la récupération des trajets.');
         }return response.json();
     }).then(data=>{handleTripResults(data)})
 }
 
 function handleTripResults(data){
     let message = document.querySelector('#right')
-    if (data.length>0){
-        message.innerHTML = '<ul>';
-        data.forEach(trip=>{
-            message.innerHTML += '<li>${trip.departure} to $ {trip.arrival} on ${trip.date}'
+    console.log(data)
+    if (data.allTrips.length>0){
+        message.innerHTML = '<div class="voyagestrain">';
+        data.allTrips.forEach(trip=>{
+            message.innerHTML += `<div class="voyage">
+            ${trip.departure}>${trip.arrival} ${trip.date} :  ${trip.price}€
+            </div> 
+            <button class="addtocart">Book</>`
         })
-        message.innerHTML += `</ul>`;
+        message.innerHTML += `</div>`;
     }
 }
-
+// `
+// <div class="row new-row">
+//           <img class="avatar" src="images/avatar-2.jpg" />
+//           <div class="text-container">
+//             <h6>Lo La</h6>
+//             <p>
+//               ${message}
+//             </p>
+//           </div>
+//           <span class="delete">✖</span>`
+//       counter();
+//       document.querySelector('#add-message').value="";
+//       for (let i = 0; i < document.querySelectorAll('.delete').length; i++) {
+//         document.querySelectorAll('.delete')[i].addEventListener('click',
+//           function () {
+//             this.parentElement.remove();
+//             counter();
+//           }
+//         );
+//        }
+//     }
 // fetch('route/vers/le/back', {
 //     method: 'Post',
 //     body: JSON.stringify()
